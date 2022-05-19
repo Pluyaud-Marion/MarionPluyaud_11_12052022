6130dc802a2c03f8c76125a3ac5a2f56d41c28ce
Marion Pluyaud <marion070189@hotmail.fr>
2022-05-19 14:36:27 +0200

    update dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,33 +1,62 @@
 import React from 'react';
 
 const Dropdown = ({ titleDropdown, propsDescription, propsEquipment }) => {
-    const title = ["Description", "Equipements"]
+    //const title = ["description", "equipements"]
+
+    function toogle(e) {
+        const description = document.getElementById("description")
+        const equipment = document.getElementById("equipments")
+        //const chevron = document.querySelector(".chevron")
+
+        console.log(e.target);
+
+        if (e.target.id === "open") {
+            description.style.display = "block"
+            e.target.setAttribute("id", "close")
+        } else if (e.target.id === "close") {
+            description.style.display = "none"
+            e.target.setAttribute("id", "open")
+        }
+
+    }
+
 
     return (
         <div className='Dropdown'>
-            <div className='container-title'>
+            <div className='container-title' id="open" onClick={(e) => toogle(e)}>
                 <span>{titleDropdown}</span>
-                {/* {
-                    title.map((el, index) => (el === "Description" ?
-                        <p key={index}>{title[0]}</p>
-                        : <p key={index}>{title[1]}</p>)
-                    )} */}
-                <img src="../arrow-up.png" alt="arrow" />
+                <img className='chevron' src="../arrow-up.png" alt="arrow" />
             </div>
-            <div>
+            <div className='test'>
                 {
-                    title.map((el, index) => (el === "Description" ?
+                    titleDropdown === "Description" ?
                         (
-                            <p key={index} className='content-description'>{propsDescription}</p>
-                        ) : (<ul className='content-equipment'>
-                            {
-                                propsEquipment?.map((list, index) => (
-                                    <li key={index}>{list}</li>
-                                ))
-                            }
-                        </ul>))
-                    )
+
+                            <p key={"content"} className='content' id='description'>{propsDescription}</p>
+                        ) : (
+                            <ul key={"list"} className='content' id='equipments'>
+                                {
+                                    propsEquipment?.map((list, index) => (
+                                        <li key={index}>{list}</li>
+                                    ))
+                                }
+                            </ul>)
                 }
+
+                {/* title.map((el, index) => (el === "description" ?
+                        (
+
+                            <p key={index} className='content'>{propsDescription}</p>
+                        ) : (
+                            <ul key={"list"} className='content'>
+                                {
+                                    propsEquipment?.map((list, index) => (
+                                        <li key={index}>{list}</li>
+                                    ))
+                                }
+                            </ul>))
+                    ) */}
+
             </div>
         </div>
     );
