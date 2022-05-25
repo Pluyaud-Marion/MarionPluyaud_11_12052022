94254a8a40e8083a3d9dd9fc238bb33418989dce
Marion Pluyaud <marion070189@hotmail.fr>
2022-05-25 18:17:59 +0200

    responsive

--- src/pages/Lodging.js
+++ src/pages/Lodging.js
@@ -47,31 +47,35 @@ const Lodging = () => {
 
     return (
         <div>
-
             {data && <CoverPicture key={data?.id} allPictures={data?.pictures} />}
-            {data && <div className='container-title-name'>
-                <h1 className='title'>{data?.title}</h1>
-                <div className='host'>
-                    <p className='name'>{data?.host?.name}</p>
-                    <img src={data?.host?.picture} alt={"photo de l'hote " + data?.host?.name} />
+            <div className='container-infos'>
+                <div className='container-infos-lodge'>
+                    {data && <div className='container-title-location'>
+                        <h1 className='title'>{data?.title}</h1>
+                        <p className='location'>{data?.location}</p>
+                    </div>}
+                    {data && <div className='container-tag'>
+                        {
+                            data?.tags?.map((tag, index) => (
+                                <Tag key={index} tag={tag} />
+                            ))
+                        }
+                    </div>}
                 </div>
 
-            </div>}
-            {data && <p className='location'>{data?.location}</p>}
-            {data && <div className='container-tag-star'>
-                <div className='container-tag'>
-                    {
-                        data?.tags?.map((tag, index) => (
-                            <Tag key={index} tag={tag} />
-                        ))
-                    }
-                </div>
-                <div className='star'>
-                    {
-                        <Star star={data?.rating} />
-                    }
-                </div>
-            </div>}
+                {data && <div className='container-host-star'>
+                    <div className='host'>
+                        <p className='name'>{data?.host?.name}</p>
+                        <img src={data?.host?.picture} alt={"photo de l'hote " + data?.host?.name} />
+                    </div>
+                    <div className='star'>
+                        {
+                            <Star star={data?.rating} />
+                        }
+                    </div>
+                </div>}
+            </div>
+
             {data && <div className='container-dropdown'>
                 <Dropdown titleDropdown="Description" contentParaph={data?.description} />
                 <Dropdown titleDropdown="Equipements" contentList={data?.equipments} />
@@ -83,3 +87,4 @@ const Lodging = () => {
 };
 
 export default Lodging;
+
